47b232d41e424f5aaaadb4b9118afa639797fbf8
Matthew Southcott <mjsouthcott@gmail.com>
2020-05-22 18:32:40 -0400

    Add User, Post, Comment and Like models

---
+++ models/like.js
@@ -0,0 +1,29 @@
+// Import dependencies
+
+// Create Like model
+module.exports = function(sequelize, DataTypes) {
+  const Like = sequelize.define("Like", {
+    // Define Post attributes
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isNumeric: true
+      }
+    },
+    comment_id: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isNumeric: true
+      }
+    }
+  });
+
+  return Like;
+};
--- models/user.js
+++ models/user.js
@@ -1,31 +1,87 @@
-// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
-var bcrypt = require("bcryptjs");
-// Creating our User model
+// Import dependencies
+const bcrypt = require("bcryptjs");
+
+// Create User model
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    // The email cannot be null, and must be a proper email before creation
-    email: {
+  const User = sequelize.define("User", {
+    // Define User attributes
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true
+        len: [6, 30],
+        isAlphanumeric: true
       }
     },
-    // The password cannot be null
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 60],
+        isAlpha: true
+      }
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 60],
+        isAlpha: true
+      }
+    },
+    profile_photo: {
+      type: DataTypes.BLOB
+    },
+    birthdate: {
+      type: DataTypes.DATE(6),
+      validate: {
+        isDate: true
+      }
+    },
+    sex: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [4, 6],
+        isAlpha: true
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [2, 60],
+        isAlpha: true
+      }
+    },
+    province: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [2, 60],
+        isAlpha: true
+      }
+    },
+    country: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [2, 60],
+        isAlpha: true
+      }
     }
   });
-  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+
+  // Create method to check if unhashed password provided by user can be compared with hashed password in database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
   };
-  // Hooks are automatic methods that run during various phases of the User Model lifecycle
-  // In this case, before a User is created, we will automatically hash their password
+  
+  // Create hook to automatically hash password provided by user prior to storing in database
   User.addHook("beforeCreate", function(user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
+
   return User;
 };
