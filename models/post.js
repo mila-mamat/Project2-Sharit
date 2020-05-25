073b593117e537366c1fad0a402d660c493aa49b
Mila <mila.windsor@gmail.com>
2020-05-25 15:51:09 -0400

    dont need get(api/posts) route

--- models/post.js
+++ models/post.js
@@ -1,6 +1,7 @@
+
 // Create Post model
 module.exports = function(sequelize, DataTypes) {
-  const Post = sequelize.define("Post", {
+ const Post = sequelize.define("Post", {
     // Define Post attributes
     post_photo: {
       type: DataTypes.STRING
